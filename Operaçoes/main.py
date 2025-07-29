42d551154658b22550c96c812327b0a39a99f6c1
joaopaulo0023 <159903749+joaopaulo0023@users.noreply.github.com>
2025-07-29 10:25:11 -0300

    Update main.py

--- Operaçoes/main.py
+++ Operaçoes/main.py
@@ -8,7 +8,7 @@ from datetime import datetime
 # ————— Configurações —————
 st.set_page_config(page_title="🔐 Login", layout="centered")
 
-DB_PATH = "usuarios.db"
+DB_PATH = os.path.join(BASE_DIR, "Operações", "usuarios.db")
 FUNCOES = [
     "Líder",
     "Separação 1",
